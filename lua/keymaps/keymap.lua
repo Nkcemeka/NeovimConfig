dd5990402e9c2c1c8ea688199bbfcc51f06eef2c
Chukwuemeka Nkama <nkcemeka@gmail.com>
2023-08-21 12:05:09 +0200

    Added Hover key mappings for LSPs

--- lua/keymaps/keymap.lua
+++ lua/keymaps/keymap.lua
@@ -7,3 +7,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+
+-- Hover Key Bindings
+vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true, silent = true})
